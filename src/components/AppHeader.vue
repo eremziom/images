1ff042632e0e14234793dd59bd4837e0e6d296f0
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-04-04 12:39:53 +0200

    project init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a href="/" class="active item">
+        IMAGE STORAGE
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
